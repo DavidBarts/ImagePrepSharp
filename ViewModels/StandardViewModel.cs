8d06800a24b4b71074bf5085ea5225f4bd4369b9
David Barts <david.w.barts@gmail.com>
2026-05-22 13:22:44 -0700

    Start adding standard menus. Update nuget packages.

--- ViewModels/StandardViewModel.cs
+++ ViewModels/StandardViewModel.cs
@@ -1,6 +1,49 @@
-﻿namespace ImagePrepSharp.ViewModels;
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ImagePrepSharp.ViewModels;
 
 public partial class StandardViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    [RelayCommand]
+    private void OpenScale()
+    {
+        System.Console.WriteLine("OpenScale clicked.");
+    }
+
+    [RelayCommand]
+    private void Preferences()
+    {
+        System.Console.WriteLine("Preferences clicked.");
+    }
+
+    [RelayCommand]
+    private void Quit()
+    {
+        System.Console.WriteLine("Quit clicked.");
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopApp)
+        {
+            desktopApp.Shutdown();
+        }
+        else
+        {
+            /* shouldn't be needed, but Justin Case */
+            System.Environment.Exit(0);
+        }
+    }
+
+    [RelayCommand]
+    private void About()
+    {
+        System.Console.WriteLine("About clicked.");
+    }
+
+    [RelayCommand]
+    private void Help()
+    {
+        System.Console.WriteLine("Help clicked.");
+    }
 }
